f80f6bd624fb675a6253379d3294edfe4060e122
OZ1NG <OZ1NG@users.noreply.github.com>
2024-08-06 11:52:18 +0000

    deploy: 8434e146eba84573e0909f6ab89bc8903ff137ba

---
+++ assets/js/data/swconf.js
@@ -0,0 +1,30 @@
+const swconf = {
+  
+    cacheName: 'chirpy-1722945138',resources: [
+      '/assets/css/jekyll-theme-chirpy.css',
+      '/',
+      
+        '/categories/',
+      
+        '/tags/',
+      
+        '/archives/',
+      
+        '/about/',
+      
+
+      
+      
+    ],
+
+    interceptor: {paths: [
+        
+      ],urlPrefixes: [
+        
+      ]
+    },
+
+    purge: false
+  
+};
+
